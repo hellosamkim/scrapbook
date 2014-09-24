6f6c710392e9f92744fdc00d7f2a2ad346420f4e
hellosamkim <sam.mt.kim@gmail.com>
2014-09-24 10:55:01 -0400

    Finished Project Euler problem 3 js

---
+++ Project Euler/Problem 3.js
@@ -0,0 +1,24 @@
+primes = [];
+number = 600851475143;
+numberSqrt = Math.round(Math.sqrt(600851475143));
+
+function isPrime(num) {
+    for (var i = 2; i < num; i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+for (var i = 2; i <= numberSqrt; i++ ) {
+    if (number % i === 0) {
+        if (isPrime(i)) {
+            primes.push(i);
+        }
+    }
+}
+
+maxPrime = Math.max.apply(null, primes);
+console.log(maxPrime);
+    
