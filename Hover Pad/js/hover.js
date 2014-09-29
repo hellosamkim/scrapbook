60a2db231397bae802ef14b10d973323aeb50d45
hellosamkim <sam.mt.kim@gmail.com>
2014-09-29 16:57:17 -0400

    Redoing the grids to make it more realistic

--- Hover Pad/js/hover.js
+++ Hover Pad/js/hover.js
@@ -2,16 +2,24 @@ $('#submit').on('click', createGrid);
 
 // Saves user's input as a variable and generates grid
 function createGrid(){
-	$('#grid').empty();
+	$('#hoverpad').empty();
 	var gridSize = document.getElementById('grid-size').value;
 	
-	if (gridSize > 50) {
-		alert('Please pick a number between 1 ~ 50')
+	if (gridSize > 100) {
+		alert('Please pick a number between 1 ~ 100')
 	};
 	generateGrid(gridSize);
 };
 
 // Generates grid according to user input's gridSize
 function generateGrid(gridSize){
-	$('#grid').append('hi')
-};
+
+	for (var i = 0; i < gridSize; i++) {
+		for (var j = 0; j < gridSize; j++) {
+			var box = $('<div class="box"></div>');
+			var size = 700 / gridSize;
+			box.css({'width': size, 'height': size})
+			$('#hoverpad').append(box);
+		}
+	}
+};	
