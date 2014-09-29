c1f12cf9c0352919a1b79720820da3876f854203
hellosamkim <sam.mt.kim@gmail.com>
2014-09-29 13:51:52 -0400

    made and fixed  functions to create/ generate grid

--- Hover Pad/js/hover.js
+++ Hover Pad/js/hover.js
@@ -1,4 +1,17 @@
-$('#submit').click(function(){
+$('#submit').on('click', createGrid);
+
+// Saves user's input as a variable and generates grid
+function createGrid(){
+	$('#grid').empty();
 	var gridSize = document.getElementById('grid-size').value;
-	alert(gridSize);
-});
+	
+	if (gridSize > 50) {
+		alert('Please pick a number between 1 ~ 50')
+	};
+	generateGrid(gridSize);
+};
+
+// Generates grid according to user input's gridSize
+function generateGrid(gridSize){
+	$('#grid').append('hi')
+};
