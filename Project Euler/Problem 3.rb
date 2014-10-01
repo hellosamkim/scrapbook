dfbcebd1b8c00eb5a3a35740b4dbf962b63fd85e
hellosamkim <sam.mt.kim@gmail.com>
2014-10-01 13:32:41 -0400

    Finished problem 3.rb for Project Euler

--- Project Euler/Problem 3.rb
+++ Project Euler/Problem 3.rb
@@ -1,1 +1,15 @@
-Problem3.rb
+require 'Prime'
+
+num = 600851475143
+num_sqrt = Math.sqrt(600851475143).round
+primes = []
+
+n = 2
+while n <= num_sqrt do
+	if num % n == 0 && Prime.prime?(n)
+		primes << n 
+	end
+	n += 1
+end
+
+p primes.max
