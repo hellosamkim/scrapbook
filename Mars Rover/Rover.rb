a85c95d11310cfe62fa56b420a1b5fa19f8455f0
hellosamkim <sam.mt.kim@gmail.com>
2014-10-17 14:20:15 -0400

    Fixed bugs, and made code more efficient

--- Mars Rover/Rover.rb
+++ Mars Rover/Rover.rb
@@ -12,25 +12,35 @@ class Rover
 		@direction = direction
 	end
 
-	def direction_output(direction)
-		case direction
-		when "N"
-			puts "North"
-		when "E"
-			puts "East"
-		when "S"
-			puts "South"
-		else
-			puts "West"
-		end
+	def to_s 
+		puts "The Rover is current at (#{@x}, #{@y}) facing " + @direction
 	end
 
-	def to_s 
-		puts "The Rover is current at (#{@x}, #{@y}) facing " + @direction.capitalize
+	def rotation(rotatedarover)
+
+	end
+
+	def movement(movedarover)
+		
+	end
+
+	def direct
+		print "Direct your Rover! (ex: LMLMRMMMLM): "
+		direct = gets.chomp
+		moves = direct.capitalize.split("")
+
+		moves.each do |move|
+			case move
+			when "L", "R"
+				rotation(move)
+			when "M"
+				movement(move)
+			end
+		end
 	end
 end
 
-class RoverStart
+class RoverGame
 	print "Please enter the max X coordinate: "
 	max_x = gets.chomp
 	print "Please enter the max Y coordinate: "
@@ -38,10 +48,10 @@ class RoverStart
 	Grid.new(max_x, max_y)
 	print "Please enter where you'd like to place your Rover (ex: 4 5 N): "
 	positions = gets.chomp
-	position = positions.split(' ')
+	position = positions.capitalize.split(' ')
 	rov1 = Rover.new(position[0],position[1],position[2])
-	puts rov1
+	rov1.direct
 end
 
-RoverStart.new
+RoverGame.new
 
