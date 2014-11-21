e352d2ae50de33db83958df838c2b85254c8c1c4
hellosamkim <sam.mt.kim@gmail.com>
2014-11-21 11:36:48 -0500

    Finished Rspec-08

--- Rspec/08_timer/timer.rb
+++ Rspec/08_timer/timer.rb
@@ -1,7 +1,11 @@
 class Timer
-  attr_accessor :seconds
+  attr_accessor :seconds, :time_string
 
   def initialize(seconds = 0)
     @seconds = seconds
+  end
+
+  def time_string
+    Time.at(@seconds).gmtime.strftime("%R:%S")
   end
 end
