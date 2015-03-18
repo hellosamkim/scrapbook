b8b57a1f64a1d26b9a44f5496caea8a8f76d9af8
hellosamkim <sam.mt.kim@gmail.com>
2015-03-18 16:06:12 -0400

    Got main layout/theme finished

--- Tic_Tac_Toe/ttt.js
+++ Tic_Tac_Toe/ttt.js
@@ -7,63 +7,64 @@ $(document).ready(function(){
     $('#player').on("click", playerGame)
     $('#computer').on("click", computerGame)
   };
-
-  function insertGrid(){
-    for (var i=0; i < 3; i++) {
-      var rows = '<div class="rows"></div>';
-      $('.game').append(rows);
-    };
-    for (var j=0; j < 3; j++) {
-      var boxes = $('<div class="boxes"></div>');
-      $('.rows').append(boxes); 
-    };
-    $('.boxes').css({
-      "border": "3px solid rgba(255, 248, 243, 0.8)", 
-    })
-    for (var x=0; x < 9; x++) {
-      $('.boxes').eq(x).attr("id", "box" + (x + 1))
-    }
-    $("#box1").css({
-      'border-radius': '15px 0 0 0'
-    });
-    $("#box3").css({
-      'border-radius': '0 15px 0 0'
-    });
-    $("#box7").css({
-      'border-radius': '0 0 0 15px'
-    });
-    $("#box9").css({
-      'border-radius': '0 0 15px 0'
-    });
-  };
-
-  function playerGame(){
-    var turn = 0;
-    var player1 = [];
-    var player2 = [];
-
-    while (player1 != "win") {
-      hoverPreview(turn)
-    }
-  };
-
-  function computerGame(){
-    
-  };
-
-  function hoverPreview(turn){
-    if (turn % 2 === 0) {
-      $('.boxes').hover(function(){
-        $(this).css('background-color', 'rgba(92, 221, 92, 0.6)')
-      }, function() {
-        $(this).css('background-color', 'rgba(255, 164, 88, 0.6)')
-      });
-    } else if (turn % 2 != 0) {
-      $('.boxes').hover(function(){
-        $(this).css('background-color', 'rgba(255, 107, 107, 0.6)')
-      }, function() {
-        $(this).css('background-color', 'rgba(255, 164, 88, 0.6)')
-      });
-    };
-  };
 });
+
+function insertGrid(){
+  for (var i=0; i < 3; i++) {
+    var rows = '<div class="rows"></div>';
+    $('.game').append(rows);
+  };
+  for (var j=0; j < 3; j++) {
+    var boxes = $('<div class="boxes"></div>');
+    $('.rows').append(boxes); 
+  };
+  for (var x=0; x < 9; x++) {
+    $('.boxes').eq(x).attr("id", "box" + (x + 1))
+  }
+  $("#box1").css({'border-radius': '15px 0 0 0'});
+  $("#box3").css({'border-radius': '0 15px 0 0'});
+  $("#box7").css({'border-radius': '0 0 0 15px'});
+  $("#box9").css({'border-radius': '0 0 15px 0'});
+};
+
+function playerGame(){
+  var turn = 0;
+  var freeSpaces = [1,2,3,4,5,6,7,8,9]
+  var player1 = [];
+  var player2 = [];
+  $('.boxes').empty()
+  playerMove(turn);
+};
+
+function computerGame(){
+  
+};
+
+function  playerMove(turn){
+  if (turn % 2 === 0){
+    console.log("A")
+    movePreLogic('rgba(255, 164, 169, 0.8)', 'X', turn);
+  } else {
+    console.log("B")
+    movePreLogic('rgba(255, 250, 164, 0.8)', 'O', turn)
+  };
+};
+
+function movePreLogic(color, sign, turn){
+  $('.boxes').hover(
+    function(){
+      if (!$(this).text()) {
+        $(this).css({'background-color': color, 'cursor': 'pointer', 'color': color})
+      } else {
+        $(this).css({'cursor': 'initial'})
+      }
+    }, function(){
+      $(this).css({'background-color': 'rgba(158, 177, 201, 0.4)', 'cursor': 'pointer'})
+    }
+  );
+  $('.boxes').on('click', function(){
+    $(this).text(sign)
+    turn++;
+    playerMove(turn);
+  });
+}
